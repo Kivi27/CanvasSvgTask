606b0f5f703ad2d27465de70bcf32b8b8c7d84ff
Kivi27 <kivi27072002@gmail.com>
2022-12-29 15:05:05 +0200

    add canvas

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@ let canvas  = document.querySelector(".canvas-picture__canvas");
 
 function drawCanvas(canvas) {
     let ctx = canvas.getContext("2d");
+    drawSun(ctx);
+}
+
+function drawSun(ctx, begX, begY) {
+    ctx.arc(begX, begY, 50, 0, 2 * Math.PI);
+    ctx.fillStyle = "yellow";
+    ctx.fill();
 
 }
 
