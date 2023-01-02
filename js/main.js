45b5a6a442cc029b19f6c50d8c6dc7f043be70a9
Kivi27 <54037162+Kivi27@users.noreply.github.com>
2023-01-02 15:51:49 +0200

    refactoring cloud

--- js/main.js
+++ js/main.js
@@ -1,77 +1,98 @@
+"use strict";
+
 let canvas  = document.querySelector(".canvas-picture__canvas");
 canvas.width = window.screen.width;
 canvas.height = 700;
 
-drawCanvas(canvas);
-function drawCanvas(canvas) {
-    let ctx = canvas.getContext("2d");
+const yellowColor = "#FAFA02";
+const blueColor = "#42F5EC";
+const greenColor = "#42f548";
+
+drawPicture(canvas);
+
+function drawPicture(canvas) {
+    const ctx = canvas.getContext("2d");
+    const heightLines = [120, 180];
+    const countCloud = 6;
+    const heightTree = [400, 600];
+    const countTree = 6;
+    const betweenDistanceDrawObject = 220;
+
     drawSun(ctx, 0, 0);
-    drawClouds(ctx);
-    drawTrees(ctx);
+    drawClouds(ctx, heightLines, countCloud, betweenDistanceDrawObject);
+    drawTrees(ctx, heightTree, countTree, betweenDistanceDrawObject);
     drawBird(ctx, 700, 200);
 }
 
 function drawSun(ctx, begX, begY) {
     const radiusSun = 80;
-    ctx.fillStyle = "yellow";
+
+    ctx.fillStyle = yellowColor;
     drawCircle(ctx, begX, begY, radiusSun);
 }
 
-function drawClouds(ctx) {
-    const heightLines = [120, 180];
-    const countCloud = 6;
-
+function drawClouds(ctx, heightLines, countCloud, betweenDistance) {
     for (let i = 0; i < countCloud; i++) {
         const heightCloud = i % 2 === 0 ? heightLines[0] : heightLines[1];
-        drawCloud(ctx, canvas.width - ((i + 1) * 220), heightCloud);
+        drawCloud(ctx, canvas.width - ((i + 1) * betweenDistance), heightCloud);
     }
 }
 
 function drawCloud(ctx, begX, begY) {
-    const colorCloud = "#42F5EC";
+    const widthCloud = 120;
+    const colorCloud = blueColor;
     const bigCircleRadius = 50;
     const smallCircleRadius = 40;
     const offsetCircleSmall = 30;
+    const beginXRectangle = begX - 65;
+    const  beginYRectangle = begY + 8;
+    const heightRectangle = 60;
+
     ctx.fillStyle = colorCloud;
     drawCircle(ctx, begX, begY, bigCircleRadius);
     drawCircle(ctx, begX - bigCircleRadius, begY + offsetCircleSmall, smallCircleRadius);
     drawCircle(ctx, begX + bigCircleRadius, begY + offsetCircleSmall, smallCircleRadius);
-    ctx.fillRect(begX-65, begY + 8, 120, 60);
+    ctx.fillRect(beginXRectangle, beginYRectangle, widthCloud, heightRectangle);
 }
 
-function drawTrees(ctx) {
-    const heightTree = [400, 600];
-    const countTree = 6;
-
+function drawTrees(ctx, heightTree, countTree, betweenDistance) {
     for (let i  = 0; i < countTree; i++) {
         const heightCloud = i % 2 === 0 ? heightTree[0] : heightTree[1];
-        drawTree(ctx, canvas.width - ((i + 1) * 220), heightCloud);
+        drawTree(ctx, canvas.width - ((i + 1) * betweenDistance), heightCloud);
     }
 }
 
 function drawTree(ctx, begX, begY) {
-    ctx.fillStyle = "#42f548";
     const sizeStem = 100;
+    const radiusCrown = 35;
+    const smallRadiusCrown = 30;
+    const treeCrowns = [
+        {offsetX: 0, offsetY: 0, radius: radiusCrown},
+        {offsetX: radiusCrown, offsetY: -20, radius: radiusCrown},
+        {offsetX: -10, offsetY: -50, radius: smallRadiusCrown},
+        {offsetX: 0, offsetY: smallRadiusCrown, radius: radiusCrown},
+        {offsetX: -40, offsetY: 0, radius: radiusCrown}
+    ];
+
+    ctx.fillStyle = greenColor;
     drawLine(ctx, begX, begY, begX, begY + sizeStem);
-    drawCircle(ctx, begX, begY, 35);
-    drawCircle(ctx, begX + 35, begY - 20, 35);
-    drawCircle(ctx, begX - 10, begY - 50, 30);
-    drawCircle(ctx, begX, begY + 30, 35);
-    drawCircle(ctx, begX - 40, begY, 35);
+    for (let treeCrown of treeCrowns) {
+        drawCircle(ctx, begX + treeCrown.offsetX, begY + treeCrown.offsetY, treeCrown.radius);
+    }
 }
 
 function drawBird(ctx, begX, begY) {
-    ctx.fillStyle = "#42f548";
     const radiusHead = 50;
     const radiusEye = 12;
     const widthBeak = 120;
     const AngleBeak = 20;
+
+    ctx.fillStyle = greenColor;
     drawCircle(ctx, begX, begY, radiusHead);
-    ctx.fillStyle = "#42F5EC";
+    ctx.fillStyle = blueColor;
     drawCircle(ctx, begX, begY - 10, radiusEye);
     drawLine(ctx, begX - radiusHead, begY, begX - widthBeak, begY + AngleBeak);
     drawLine(ctx, begX - radiusHead + 5, begY + AngleBeak, begX - widthBeak, begY + AngleBeak);
-    ctx.fillStyle = "#42F5EC";
     const centerBodyX = begX + radiusHead + 50;
     const centerBodyY = begY + radiusHead * 2;
     const radiusBodyX = 100;
